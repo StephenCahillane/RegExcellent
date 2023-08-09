d0ca4123463a62528f988e9fe9ac55d70a2fb07e
Jonah <jydelaware@gmail.com>
2023-08-09 10:34:04 -0400

    change post method name

---
+++ src/main/webapp/javascript/Game.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import React, { useState, useEffect } from "react";
+
+export default function Game(){
+    const [input, setInput] = useState([]);
+
+    return(
+        <canvas id="canvas">
+            
+        </canvas>
+    );
+}
