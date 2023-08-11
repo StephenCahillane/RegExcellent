abe93ff3aae58d469940edeae5f478b5fd85cd55
Jonah <jydelaware@gmail.com>
2023-08-10 23:24:14 -0400

    Main Menu

--- src/main/webapp/javascript/Canvas.jsx
+++ src/main/webapp/javascript/Canvas.jsx
@@ -5,60 +5,57 @@ export default function Canvas({width, height}){
     const [input, setInput] = useState([]);
     const canvasRef = useRef(null);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
+    // useEffect(() => {
+    //     const canvas = canvasRef.current;
+    //     const context = canvas.getContext('2d');
 
-        const keyDownHandler = (e) => {
-            if((e.key == "ArrowDown" || e.key == "ArrowUp" || e.key == "ArrowLeft" ||  e.key == "ArrowRight") && input.indexOf(e.key) === -1)
-            {
-                input.push(e.key);
-                console.log(input);
-            }
-        }
+    //     const keyDownHandler = (e) => {
+    //         if((e.key == "ArrowDown" || e.key == "ArrowUp" || e.key == "ArrowLeft" ||  e.key == "ArrowRight") && input.indexOf(e.key) === -1)
+    //         {
+    //             input.push(e.key);
+    //             console.log(input);
+    //         }
+    //     }
 
-        const keyUpHandler = (e) => {
-            if((e.key == "ArrowDown" || e.key == "ArrowUp" || e.key == "ArrowLeft" || e.key == "ArrowRight")){
-                input.splice(input.indexOf(e.key), 1);
-                console.log(input);
-            }
-        }
+    //     const keyUpHandler = (e) => {
+    //         if((e.key == "ArrowDown" || e.key == "ArrowUp" || e.key == "ArrowLeft" || e.key == "ArrowRight")){
+    //             input.splice(input.indexOf(e.key), 1);
+    //             console.log(input);
+    //         }
+    //     }
         
 
-        canvas.addEventListener("keydown", keyDownHandler);
-        canvas.addEventListener("keyup", keyUpHandler);
+    //     canvas.addEventListener("keydown", keyDownHandler);
+    //     canvas.addEventListener("keyup", keyUpHandler);
 
-        return () => {
-            canvas.removeEventListener("keydown", keyDownHandler);
-            canvas.removeEventListener("keyup", keyUpHandler);
-        };
-    }, []);
+    //     return () => {
+    //         canvas.removeEventListener("keydown", keyDownHandler);
+    //         canvas.removeEventListener("keyup", keyUpHandler);
+    //     };
+    // }, []);
 
-    function Player({gameWidth, gameHeight, context}){
-        const [width, setWidth] = useState(100);
-        const [height, setHeight] = useState(100);
-        const [x, setX] = useState(0);
-        const [y, setY] = useState(0);
+    // function Player({gameWidth, gameHeight, context}){
+    //     const [width, setWidth] = useState(100);
+    //     const [height, setHeight] = useState(100);
+    //     const [x, setX] = useState(0);
+    //     const [y, setY] = useState(0);
 
-        setY(gameHeight - height);
+    //     setY(gameHeight - height);
 
-        function draw(context){
-            context.fillStyle = "black";
-            context.fillRect(x, y, width, height);
-        }
+    //     function draw(context){
+    //         context.fillStyle = "black";
+    //         context.fillRect(x, y, width, height);
+    //     }
 
-        function update(){
-            setX(x++);
-        }
+    //     function update(){
+    //         setX(x++);
+    //     }
 
-        draw(context);
+    //     draw(context);
+
+        return(
+            <div>
+                <></>
+            </div>
+        );
     }
-
-    return(
-        <div>
-            <Canvas tabIndex="-1" ref={canvasRef} width={width} height={height}>
-                
-            </Canvas>
-        </div>
-    );
-}
