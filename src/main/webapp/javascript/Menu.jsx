60093c6483ecb06dc79534fcf831fb814fd3d385
Jonah <jydelaware@gmail.com>
2023-08-13 14:29:24 -0400

    Add candles to menu

--- src/main/webapp/javascript/Menu.jsx
+++ src/main/webapp/javascript/Menu.jsx
@@ -14,15 +14,23 @@ export default function Menu(){
         return (
             <div>
                 <h1 className="menu-header">RegQuest</h1>
-                <div className="btn-container">
-                    <Link to="/Maze" >
-                        <button className="menu-btn" onClick={toggleActive}>Begin Quest</button>
-                    </Link>
-                        <button className="menu-btn" onClick={toggleActive}>What is a regular expression?</button>
-                    <Link to="/About">
-                        <button className="menu-btn" onClick={toggleActive}>About the Game</button>
-                    </Link>
-                    <button className="menu-btn" onClick={toggleActive}>Quit</button>
+                <div className="menu-container">
+                    <div>
+                        <img src="images/animated-candle.gif"></img>
+                    </div>
+                    <div className="btn-container">
+                        <Link to="/Maze" >
+                            <button className="menu-btn" onClick={toggleActive}>Begin Quest</button>
+                        </Link>
+                            <button className="menu-btn" onClick={toggleActive}>What is a regular expression?</button>
+                        <Link to="/About">
+                            <button className="menu-btn" onClick={toggleActive}>About the Game</button>
+                        </Link>
+                        <button className="menu-btn" onClick={toggleActive}>Quit</button>
+                    </div>
+                    <div>
+                        <img src="images/animated-candle.gif"></img>
+                    </div>
                 </div>
             </div>
         );
