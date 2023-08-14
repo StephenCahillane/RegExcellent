fc738853a1ada846c087abe53ebdf4cd1f5ad093
Jonah <jydelaware@gmail.com>
2023-08-14 10:23:27 -0400

    clean white space

--- src/main/webapp/javascript/Slider.jsx
+++ src/main/webapp/javascript/Slider.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import '../css/style.css';
 
 
-
-
 export default function Sliding() {
 
   const [data, setData] = useState([[]]);
@@ -15,14 +13,8 @@ export default function Sliding() {
   const [width, setWidth] = useState(5);
 
 
-
   useEffect(() => {
-    //   setData(
-    //     new Array(rows)
-    //       .fill(undefined)
-    //       .map((_, row) => new Array(columns).fill(undefined).map((_, column) => row * column))
-    //   );
-    // }, [rows, columns]);
+    
     function cell(row, col) {
       let trap = undefined;
       if (col % 3 === 0 && col != 0) {
@@ -64,7 +56,7 @@ export default function Sliding() {
             <tr key={`Row${bottom + rowIdx}`}>
               {dataRow.slice(left, left + width).map((cell, columnIdx) => (
                 <td key={`Col${left + columnIdx}`}>
-                  <Cell cellData={cell} />
+                  <Cell cellData={cell} bottom={bottom} left={left} />
                 </td>
               ))}
               
@@ -88,8 +80,8 @@ function Knob({ getter, setter, text }) {
 }
 
 
-export function Cell({ cellData }) {
-  const isFirstCell = cellData.row === 0 && cellData.col === 0;
+export function Cell({ cellData, bottom, left }) {
+  const isFirstCell = cellData.row === bottom && cellData.col === left;
 
   return (
     <div>
