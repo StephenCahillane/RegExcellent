78fce823d9e2635ddd647e90fb33437ecb675937
Mckeon-Campbell <mckeoncampbell@gmail.com>
2023-08-23 10:13:11 -0400

    typos

--- src/main/webapp/javascript/About.jsx
+++ src/main/webapp/javascript/About.jsx
@@ -24,7 +24,7 @@ export default function About() {
           then filled each cell with data such as the column the player is
           currently in, and trap cells that we populated with trapID's. These
           trap ID's are linked with specific pictures and questions that will
-          prompt when encountering teh trap cell. We used ReactSimple Animate to
+          prompt when encountering the trap cell. We used ReactSimple Animate to
           implement animations of the knight walking in place, as well as CSS
           animate to make the movement between cells fluid.
         </p>
